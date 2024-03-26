a0d4f930bbbb9d053ad786c2843f6dea9fcc8dd6
Estherifeoma1 <esthernnamani111@gmail.com>
2024-03-26 11:15:34 -0700

    first commit

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: firest int
+ * @b: second int
+ * Return: addition 
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiply two integers
+ * @a: firest int
+ * @b: second int
+ * Return: multiply 
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mod of two integers
+ * @a: firest int
+ * @b: second int
+ * Return: final answer 
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - subtract two integers
+ * @a: firest int
+ * @b: second int
+ * Return: subtract the final result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
